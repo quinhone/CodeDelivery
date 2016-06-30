c7ac14dd39dd546bfda6f012e04de87ee24107fd
quinhone <lcquinhone@gmail.com>
2016-06-30 13:31:31 -0400

    Correção de erro

--- app/Transformers/OrderItemTransformer.php
+++ app/Transformers/OrderItemTransformer.php
@@ -11,26 +11,27 @@ use CodeDelivery\Entities\OrderItem;
  */
 class OrderItemTransformer extends TransformerAbstract
 {
-    protected $defaultIncludes = ['product'];
+	protected $defaultIncludes = [ 'product' ];
 
-    /**
-     * Transform the \OrderItem entity
-     * @param \OrderItem $model
-     *
-     * @return array
-     */
-    public function transform(OrderItem $model)
-    {
-        return [
-            'id'    => (int) $model->id,
-            'product_id' => (int) $model->product_id,
-            'price' => $model->price,
-            'qtd'   => $model->qtd
-        ];
-    }
+	/**
+	 * Transform the \OrderItem entity
+	 * @param \OrderItem $model
+	 *
+	 * @return array
+	 */
+	public function transform ( OrderItem $model )
+	{
+		return [
+			'id' => (int)$model->id,
+			'product_id' => (int)$model->product_id,
+			'price' => $model->price,
+			'qtd' => $model->qtd
+		];
+	}
 
-    public function includeProduct(OrderItem $model)
-    {
-        return $this->item($model->items, new ProductTransformer());
-    }
+	public function includeProduct ( OrderItem $model )
+	{
+		return $this->item ( $model->product, new ProductTransformer() );
+	}
+	
 }
